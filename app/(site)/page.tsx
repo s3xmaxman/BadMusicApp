db0de21c55f49d59f63ac0126d748e90669f060e
s3xmaxman <badside3919@gmail.com>
2023-12-05 15:03:06 +0900

    first commit

---
+++ app/(site)/page.tsx
@@ -0,0 +1,14 @@
+import Header from '@/components/Header'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className="bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto">
+      <Header>
+
+      </Header>
+    </div>
+  )
+}
+
+export default page
