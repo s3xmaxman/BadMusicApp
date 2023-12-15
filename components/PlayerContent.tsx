1e027831c36117e7ffca92aa31659c9914ca26b0
s3xmaxman <badside3919@gmail.com>
2023-12-16 07:19:40 +0900

    bug fix

--- components/PlayerContent.tsx
+++ components/PlayerContent.tsx
@@ -6,6 +6,7 @@ import { BsPauseFill, BsPlayFill } from "react-icons/bs";
 import { HiSpeakerWave, HiSpeakerXMark } from "react-icons/hi2";
 import { AiFillStepBackward, AiFillStepForward } from "react-icons/ai";
 import { BsRepeat1 } from "react-icons/bs";
+import { FaRandom } from "react-icons/fa";
 
 import { Song } from "@/types";
 import usePlayer from "@/hooks/usePlayer";
@@ -19,19 +20,26 @@ import Seekbar from './Seekbar';
 interface PlayerContentProps {
   song: Song;
   songUrl: string;
+  
 }
 
 const PlayerContent: React.FC<PlayerContentProps> = ({ 
   song, 
-  songUrl
+  songUrl,
 }) => {
     const player = usePlayer();
     const [volume, setVolume] = useState(0.1);
     const [isPlaying, setIsPlaying] = useState(false);
     const [isRepeating, setIsRepeating] = useState(false);
-    const [playbackTime, setPlaybackTime] = useState(0);
-    const [duration, setDuration] = useState(0);
+    const [isShuffling, setIsShuffling] = useState(false);
     const isRepeatingRef = useRef(isRepeating);
+
+    console.log(songUrl)
+
+    // song-Nocturnal-lq2lzdez
+    
+
+    
 
    
 
@@ -44,29 +52,39 @@ const PlayerContent: React.FC<PlayerContentProps> = ({
       if (player.ids.length === 0) {
         return;
       }
-  
+    
+      if (isShuffling) {
+        const randomIndex = Math.floor(Math.random() * player.ids.length);
+        return player.setId(player.ids[randomIndex]);
+      }
+      
       const currentIndex = player.ids.findIndex((id) => id === player.activeId);
       const nextSong = player.ids[currentIndex + 1];
-  
+    
       if (!nextSong) {
         return player.setId(player.ids[0]);
       }
-  
+    
       player.setId(nextSong);
     }
-  
+    
     const onPlayPrevious = () => {
       if (player.ids.length === 0) {
         return;
       }
-  
+      
+      if (isShuffling) {
+        const randomIndex = Math.floor(Math.random() * player.ids.length);
+        return player.setId(player.ids[randomIndex]);
+      }
+    
       const currentIndex = player.ids.findIndex((id) => id === player.activeId);
       const previousSong = player.ids[currentIndex - 1];
-  
+    
       if (!previousSong) {
         return player.setId(player.ids[player.ids.length - 1]);
       }
-  
+    
       player.setId(previousSong);
     }
 
@@ -74,8 +92,6 @@ const PlayerContent: React.FC<PlayerContentProps> = ({
     const [play, {
       pause,
       sound,
-      duration: soundDuration,
-      stop
      }] = useSound(
       songUrl,
       { 
@@ -92,7 +108,6 @@ const PlayerContent: React.FC<PlayerContentProps> = ({
         },
         onpause: () => setIsPlaying(false),
         format: ['mp3'],
-        onload: () => setDuration(soundDuration), // ロード時に総時間をセット
       }
      );
 
@@ -100,42 +115,26 @@ const PlayerContent: React.FC<PlayerContentProps> = ({
       isRepeatingRef.current = isRepeating;
     }, [isRepeating]);
     
-
-  
-     
-    const formatTime = (seconds: number): string => {
-      const pad = (num: number, size: number): string => num.toString().padStart(size, '0');
-      const minutes = pad(Math.floor(seconds / 60), 2);
-      const secondsLeft = pad(Math.abs(Math.floor(seconds % 60)), 2); 
-      return `${minutes}:${secondsLeft}`;
-      };
-      
-      const currentTime = formatTime(playbackTime);
-      const remainingTime = formatTime(duration - playbackTime);
-
-      useEffect(() => {
-        const interval = setInterval(() => {
-          if (sound?.playing()) {
-            setPlaybackTime(Math.round(sound.seek()));
-          }
-        }, 1000); // 1秒ごとに更新
-      
-        return () => {
-          clearInterval(interval);
-        };
-      }, [isPlaying, sound]);
-      
-       
+   
     // コンポーネントがアンマウントされるときにサウンドをアンロードします。
-      useEffect(() => {
+    if(isRepeating) {
+        useEffect(() => {
           sound?.play(); // サウンドがあれば再生します。
           
           return () => {
           sound?.unload(); // コンポーネントのクリーンアップ時にサウンドをアンロードします。
           }
       }, [isPlaying, sound]);
-
-
+    } else {
+        useEffect(() => {
+            sound?.play(); // サウンドがあれば再生します。
+            
+            return () => {
+            sound?.unload(); // コンポーネントのクリーンアップ時にサウンドをアンロードします。
+            }
+        }, [sound]);
+    } 
+          
      // 再生ボタンのハンドラです。再生中ではない場合は再生を開始し、そうでなければ一時停止します。
      const handlePlay = () => {
       if (!isPlaying) {
@@ -145,7 +144,6 @@ const PlayerContent: React.FC<PlayerContentProps> = ({
       }
     }  
        
-
     // ミュート切り替え関数です。現在ミュートされていれば音量を戻し、そうでなければミュートします。
     const toggleMute = () => {
         if (volume === 0) {
@@ -159,6 +157,12 @@ const PlayerContent: React.FC<PlayerContentProps> = ({
       setIsRepeating(!isRepeating);
       console.log('Repeat status changed to:', !isRepeating);
     };
+
+    const toggleShuffle = () => {
+      setIsShuffling(!isShuffling);
+      console.log('Shuffle status changed to:', !isShuffling);
+     
+    }
 
     return ( 
       <div className="grid grid-cols-2 md:grid-cols-3 h-full">
@@ -209,7 +213,12 @@ const PlayerContent: React.FC<PlayerContentProps> = ({
               gap-x-6
             "
           >
-            <span>{currentTime}</span>
+            <FaRandom
+             onClick={toggleShuffle}
+             size={20}
+             className= "text-neutral-400 cursor-pointer hover:text-white transition" 
+             style={{color: isShuffling ? 'green' : 'white'}}
+            />
             <AiFillStepBackward
               onClick={onPlayPrevious}
               size={30} 
@@ -273,3 +282,4 @@ const PlayerContent: React.FC<PlayerContentProps> = ({
 }
  
 export default PlayerContent;
+
