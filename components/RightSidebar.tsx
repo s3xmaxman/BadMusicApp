b86082d18b5cb8ca2ba37a9a4abf3e8434d6f5af
s3xmaxman <badside3919@gmail.com>
2024-03-29 02:30:45 +0900

    change RightSidebar

--- components/RightSidebar.tsx
+++ components/RightSidebar.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image';
 import useLoadImage from '@/hooks/useLoadImage';
 import usePlayer from '@/hooks/usePlayer';
 import useGetSongById from '@/hooks/useGetSongById';
+import { FaMusic } from 'react-icons/fa';
 
 
 const RightSidebar = () => {
@@ -14,21 +15,25 @@ const RightSidebar = () => {
   if (!song) {
     return null;
   }
-
+  
   return (
-    <div className="bg-neutral-900 text-white p-4 h-full flex flex-col">
-      <div className="flex-grow relative">
+    <div className="bg-gradient-to-b from-violet-950 text-white p-4 h-full flex flex-col items-center rounded-lg">
+      <div className="relative w-full max-w-xs mx-auto">
         <Image
           src={imagePath || '/images/RightSide.png'}
           alt="Song Image"
           width={600}
           height={600}
-          className="w-full object-cover rounded-lg mx-auto"
+          className="w-full object-cover rounded-lg shadow-lg transition-all duration-500 ease-in-out"
+          style={{ filter: 'brightness(0.8)' }}
         />
+        <div className="absolute inset-0 flex items-center justify-center opacity-0 hover:opacity-100 transition-opacity duration-300">
+          <FaMusic className="text-white text-6xl" />
+        </div>
       </div>
-      <div className="mt-4 text-right">
-        <h1 className="text-4xl font-bold">{song.title}</h1>
-        <p className="text-xl">{song.author}</p>
+      <div className="mt-4 text-center">
+        <h1 className="text-3xl font-bold tracking-wide">{song.title}</h1>
+        <p className="text-lg text-gray-300">{song.author}</p>
       </div>
     </div>
   );
