b46e5c938b5111e6f23b28579a6a0dccebef3c00
s3xmaxman <badside3919@gmail.com>
2024-03-28 15:22:58 +0900

    fix code

--- components/MediaItem.tsx
+++ components/MediaItem.tsx
@@ -29,26 +29,9 @@ const MediaItem: React.FC<MediaItemProps> = ({
   return ( 
     <div
       onClick={handleClick}
-      className="
-        flex 
-        items-center 
-        gap-x-3 
-        cursor-pointer 
-        hover:bg-neutral-800/50 
-        w-full 
-        p-2 
-        rounded-md
-      "
+      className="flex items-center gap-x-3 cursor-pointer hover:bg-neutral-800/50 p-2 rounded-md"
     >
-      <div 
-        className="
-          relative 
-          rounded-md 
-          min-h-[48px] 
-          min-w-[48px] 
-          overflow-hidden
-        "
-      >
+      <div className=" relative rounded-md overflow-hidden min-h-[48px] min-w-[48px] ">
         <Image
           fill
           src={imageUrl || "/images/music-placeholder.png"}
