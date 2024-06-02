c2f194948fc6418326d29ab8d12a64283eb8eb59
s3xmaxman <badside3919@gmail.com>
2024-06-03 02:24:45 +0900

    update PageContent.tsx, page.tsx and PlayerContent.tsx

--- app/(site)/components/PageContent.tsx
+++ app/(site)/components/PageContent.tsx
@@ -1,32 +1,35 @@
-"use client"
+"use client";
 
-import SongItem from "@/components/SongItem"
-import useOnPlay from "@/hooks/useOnPlay"
-import { Song } from "@/types"
+import SongItem from "@/components/SongItem";
+import useOnPlay from "@/hooks/useOnPlay";
+import { Song } from "@/types";
 
 interface PageContentProps {
-    songs: Song[]
+  songs: Song[];
 }
 
-const PageContent: React.FC<PageContentProps> = ({songs}) => {
-  
+const PageContent: React.FC<PageContentProps> = ({ songs }) => {
   const onPlay = useOnPlay(songs);
 
   if (!songs) {
     return (
-        <div className="mt-4 text-neutral-400">
-            <h1>Loading...</h1>
-        </div>
-    )
+      <div className="mt-4 text-neutral-400">
+        <h1>Loading...</h1>
+      </div>
+    );
   }
 
   return (
     <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4 mt-3">
       {songs.map((item) => (
-        <SongItem onClick={(id: string) => onPlay(id)} key={item.id} data={item} />
+        <SongItem
+          onClick={(id: string) => onPlay(id)}
+          key={item.id}
+          data={item}
+        />
       ))}
     </div>
-  )
-}
+  );
+};
 
-export default PageContent
+export default PageContent;
--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,11 +1,10 @@
-import getSongs from '@/actions/getSongs';
-import Header from '@/components/Header'
-import ListItem from '@/components/ListItem'
-import PageContent from './components/PageContent';
-import RightSidebar from '@/components/RightSidebar';
+import getSongs from "@/actions/getSongs";
+import Header from "@/components/Header";
+import ListItem from "@/components/ListItem";
+import PageContent from "./components/PageContent";
+import RightSidebar from "@/components/RightSidebar";
 
-
-export const revalidate = 0
+export const revalidate = 0;
 
 export default async function Home() {
   const songs = await getSongs();
@@ -13,27 +12,31 @@ export default async function Home() {
     <div className="flex bg-neutral-900 rounded-lg h-full overflow-hidden">
       <div className="w-[1250px] h-full overflow-y-auto">
         <Header>
-          <div className='mb-2'>
-            <h1 className='text-white text-3xl font-semibold'> こんにちは </h1>
-            <div className=' grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3 mt-4'>
-              <ListItem image='/images/liked.png' name='お気に入りの曲' href='liked' />
+          <div className="mb-2">
+            <h1 className="text-white text-3xl font-semibold"> こんにちは </h1>
+            <div className=" grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3 mt-4">
+              <ListItem
+                image="/images/liked.png"
+                name="お気に入りの曲"
+                href="liked"
+              />
             </div>
           </div>
         </Header>
-        <div className='mt-2 mb-7 px-6'>
-          <div className='flex justify-between items-center'>
-            <h1 className='text-white text-2xl font-semibold'> 最新曲 </h1>
+        <div className="mt-2 mb-7 px-6">
+          <div className="flex justify-between items-center">
+            <h1 className="text-white text-2xl font-semibold"> 最新曲 </h1>
           </div>
-          <div className='flex flex-col md:flex-row'>
-            <div className='w-full md:w-auto md:mr-4'>
+          <div className="flex flex-col md:flex-row">
+            <div className="w-full md:w-auto md:mr-4">
               <PageContent songs={songs} />
             </div>
           </div>
         </div>
       </div>
-      <div className="hidden md:block h-full w-96 overflow-y-auto ">
+      <div className="hidden lg:block h-full w-96 overflow-y-auto ">
         <RightSidebar />
       </div>
     </div>
-  )
+  );
 }
