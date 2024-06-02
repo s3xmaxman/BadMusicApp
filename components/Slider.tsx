009e97625180aa0a1f25b17ed70ef7a929353938
s3xmaxman <badside3919@gmail.com>
2024-06-03 01:34:36 +0900

    update Slider.tsx

--- components/Slider.tsx
+++ components/Slider.tsx
@@ -1,6 +1,6 @@
 "use client";
-
 import * as RadixSlider from "@radix-ui/react-slider";
+import React from "react";
 
 interface SliderProps {
   value?: number;
@@ -14,15 +14,7 @@ const Slider: React.FC<SliderProps> = ({ value = 1, onChange }) => {
 
   return (
     <RadixSlider.Root
-      className="
-        relative 
-        flex 
-        items-center 
-        select-none 
-        touch-none 
-        w-full 
-        h-10
-      "
+      className=" relative flex items-center select-none touch-none w-full h-10"
       defaultValue={[1]}
       value={[value]}
       onValueChange={handleChange}
@@ -30,23 +22,8 @@ const Slider: React.FC<SliderProps> = ({ value = 1, onChange }) => {
       step={0.1}
       aria-label="Volume"
     >
-      <RadixSlider.Track
-        className="
-          bg-neutral-600 
-          relative 
-          grow 
-          rounded-full 
-          h-[3px]
-        "
-      >
-        <RadixSlider.Range
-          className="
-            absolute 
-            bg-white 
-            rounded-full 
-            h-full
-          "
-        />
+      <RadixSlider.Track className=" relative grow rounded-full h-[6px] bg-gray-300">
+        <RadixSlider.Range className=" absolute rounded-full h-full bg-purple-500" />
       </RadixSlider.Track>
     </RadixSlider.Root>
   );
