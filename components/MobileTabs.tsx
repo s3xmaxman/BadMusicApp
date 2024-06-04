cfc0013e99eac2b921c2be38f19bb9424de0717b
s3xmaxman <badside3919@gmail.com>
2024-06-04 21:27:01 +0900

    update MobileTabs.tsx

--- components/MobileTabs.tsx
+++ components/MobileTabs.tsx
@@ -1,12 +1,7 @@
 "use client";
 import Link from "next/link";
 import React, { useState } from "react";
-import {
-  AiFillHome,
-  AiOutlinePlusCircle,
-  AiOutlineBars,
-  AiOutlineSearch,
-} from "react-icons/ai";
+import { AiFillHome, AiOutlineBars, AiOutlineSearch } from "react-icons/ai";
 
 const MobileTabs = () => {
   const [activeTab, setActiveTab] = useState("home");
