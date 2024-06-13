c7abb8090cb13971163688b67117074bfd252394
s3xmaxman <badside3919@gmail.com>
2024-06-13 15:09:19 +0900

    update TrendBoard.tsx

--- components/TrendBoard.tsx
+++ components/TrendBoard.tsx
@@ -2,6 +2,8 @@
 import { Song } from "@/types";
 import useLoadImages from "@/hooks/useLoadImages";
 import Image from "next/image";
+import Link from "next/link";
+import useOnPlay from "@/hooks/useOnPlay";
 
 interface TrendBoardProps {
   trendSongs: Song[];
@@ -9,6 +11,7 @@ interface TrendBoardProps {
 
 const TrendBoard: React.FC<TrendBoardProps> = ({ trendSongs }) => {
   const imageUrls = useLoadImages(trendSongs);
+  const onPlay = useOnPlay(trendSongs);
 
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-8 p-4">
@@ -17,7 +20,7 @@ const TrendBoard: React.FC<TrendBoardProps> = ({ trendSongs }) => {
           key={song.id}
           className="bg-gradient-to-br from-gray-800 to-gray-900 rounded-xl overflow-hidden shadow-lg transform transition-transform duration-300 hover:scale-105 hover:shadow-2xl"
         >
-          <div className="relative w-full h-60">
+          <div onClick={() => onPlay(song.id)} className="relative w-full h-60">
             <Image
               src={imageUrls[index] || "/images/liked.png"}
               alt={song.title}
@@ -29,7 +32,11 @@ const TrendBoard: React.FC<TrendBoardProps> = ({ trendSongs }) => {
           <div className="p-4">
             <h3 className="text-white text-xl font-bold mb-1 flex items-center space-x-2">
               <span className="text-green-400">#{index + 1}</span>
-              <span>{song.title}</span>
+              <Link href={`/songs/${song.id}`}>
+                <span className="cursor-pointer hover:underline">
+                  {song.title}
+                </span>
+              </Link>
             </h3>
             <p className="text-gray-400 text-sm">{song.author}</p>
             <p className="text-gray-500 text-xs mt-2">{song.count} plays</p>
