6ae9b64764e29343524233dfab51eecdfe141d04
s3xmaxman <badside3919@gmail.com>
2024-06-14 13:07:08 +0900

    update SongItem.tsx

--- components/SongItem.tsx
+++ components/SongItem.tsx
@@ -5,6 +5,7 @@ import { Song } from "@/types";
 import Image from "next/image";
 import Link from "next/link";
 import PlayButton from "./PlayButton";
+import { CiPlay1 } from "react-icons/ci";
 
 interface SongItemProps {
   onClick: (id: string) => void;
@@ -16,21 +17,21 @@ const SongItem: React.FC<SongItemProps> = ({ onClick, data }) => {
   return (
     <div
       className="
-      relative 
-      group 
-      flex 
-      flex-col 
-      items-center 
-      justify-center 
-      rounded-md 
-      overflow-hidden 
-      gap-x-4 
-      bg-neutral-400/5 
-      cursor-pointer 
-      hover:bg-neutral-400/10 
-      transition 
-      p-3
-      "
+        relative 
+        group 
+        flex 
+        flex-col 
+        items-center 
+        justify-center 
+        rounded-md 
+        overflow-hidden 
+        gap-x-4 
+        bg-neutral-400/5 
+        cursor-pointer 
+        hover:bg-neutral-400/10 
+        transition 
+        p-3
+        "
     >
       <div className="relative aspect-square w-full h-full rounded-md overflow-hidden">
         <Image
@@ -41,7 +42,7 @@ const SongItem: React.FC<SongItemProps> = ({ onClick, data }) => {
           onClick={() => onClick(data.id)}
         />
       </div>
-      <div className="flex flex-col items-start w-full pt-4 gap-y-1">
+      <div className="flex flex-col items-start justify-between w-full pt-4 gap-y-1">
         <Link href={`/songs/${data.id}`} className="w-full">
           <p className="font-semibold truncate w-full hover:underline">
             {data.title}
@@ -50,8 +51,12 @@ const SongItem: React.FC<SongItemProps> = ({ onClick, data }) => {
         <p className="text-neutral-400 text-sm pb-4 w-full truncate">
           {data.author}
         </p>
+        <div className="flex items-center self-end">
+          <CiPlay1 />
+          <div className="text-white ml-1">{data.count}</div>
+        </div>
       </div>
-      <div className="absolute bottom-24 right-5">
+      <div className="absolute bottom-35 right-5">
         <PlayButton />
       </div>
     </div>
