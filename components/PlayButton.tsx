3a4732a588c93856dcec6553cec12615aefd3809
s3xmaxman <badside3919@gmail.com>
2024-06-14 14:23:33 +0900

    build(deps): update PlayButton.tsx, useOnPlay.ts, package-lock.json and package.json

--- components/PlayButton.tsx
+++ components/PlayButton.tsx
@@ -10,7 +10,7 @@ const PlayButton = () => {
             rounded-full
             flex
             items-center
-            bg-green-500
+            bg-[#4c1d95]
             p-4
             drop-shadow-md
             translate
