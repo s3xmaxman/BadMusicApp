ff939a3d291e67381155e1760843c14206411489
s3xmaxman <badside3919@gmail.com>
2024-10-23 00:38:00 +0900

    update page.tsx and SoundCloudPlayerContent.tsx

--- components/SoundCloudPlayerContent.tsx
+++ components/SoundCloudPlayerContent.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef } from "react";
 import { BsPauseFill, BsPlayFill, BsRepeat1 } from "react-icons/bs";
 import { FaRandom } from "react-icons/fa";
 import { AiFillStepBackward, AiFillStepForward } from "react-icons/ai";
