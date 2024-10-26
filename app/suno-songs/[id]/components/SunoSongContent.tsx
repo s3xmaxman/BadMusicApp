60fbc2825ec38feadcbb92421f1e67f8757b8196
s3xmaxman <badside3919@gmail.com>
2024-10-26 19:08:02 +0900

    update page.tsx

---
+++ app/suno-songs/[id]/components/SunoSongContent.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+interface SunoSongContentProps {
+  sunoSongId: string;
+}
+
+const SunoSongContent = ({ sunoSongId }: SunoSongContentProps) => {
+  return <div>SunoSongContent</div>;
+};
+
+export default SunoSongContent;
