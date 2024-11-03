1216eda7892f32c5f2b010adfac68e2e26131b68
s3xmaxman <badside3919@gmail.com>
2024-11-04 00:34:47 +0900

    update MobileStyleIcons.tsx

--- components/Mobile/MobileStyleIcons.tsx
+++ components/Mobile/MobileStyleIcons.tsx
@@ -25,16 +25,18 @@ const MobileStyleIcons: React.FC<MobileStyleIconsProps> = memo(
         <span className="text-xs mt-1">Lyrics</span>
       </button>
 
-      {songType === "regular" && (
-        <div className="flex flex-col items-center text-white">
-          <div className="w-12 h-12 bg-black bg-opacity-50 rounded-full flex items-center justify-center">
-            <AddPlaylist playlists={playlists} songId={songId}>
-              <RiPlayListAddFill size={24} />
-            </AddPlaylist>
-          </div>
-          <span className="text-xs mt-1">Playlist</span>
+      <div className="flex flex-col items-center text-white">
+        <div className="w-12 h-12 bg-black bg-opacity-50 rounded-full flex items-center justify-center">
+          <AddPlaylist
+            playlists={playlists}
+            songId={songId}
+            songType={songType}
+          >
+            <RiPlayListAddFill size={24} />
+          </AddPlaylist>
         </div>
-      )}
+        <span className="text-xs mt-1">Playlist</span>
+      </div>
 
       <div className="flex flex-col items-center text-white">
         <div className="w-12 h-12 bg-black bg-opacity-50 rounded-full flex items-center justify-center">
