e653837a2c085445f36b83b508da4b96778641b0
s3xmaxman <badside3919@gmail.com>
2025-02-18 05:40:53 +0900

    feat: Use environment variables for R2 storage configuration
    This commit updates the R2 storage configuration to use environment variables instead of hardcoded values.
    
    Changes include:
    
    - Modified uploadFileToR2 action to use NEXT_PUBLIC_R2_ENDPOINT, NEXT_PUBLIC_R2_ACCESS_KEY, and NEXT_PUBLIC_R2_SECRET_KEY environment variables for R2 storage configuration.
    - Removed hardcoded R2 storage configuration.

--- actions/uploadFileToR2.ts
+++ actions/uploadFileToR2.ts
@@ -26,18 +26,12 @@ const uploadFileToR2 = async ({
     file.name
   )}-${Date.now()}`;
 
-  console.log("R2_ACCOUNT_ID:", process.env.R2_ACCOUNT_ID);
-  console.log("R2_ACCESS_KEY:", process.env.R2_ACCESS_KEY);
-  console.log("R2_SECRET_KEY:", process.env.R2_SECRET_KEY);
-
   const s3Client = new S3Client({
     region: "auto",
-    endpoint:
-      "https://42995ddbe3fdf60d4dd3fa0f60343689.r2.cloudflarestorage.com",
+    endpoint: process.env.NEXT_PUBLIC_R2_ENDPOINT!,
     credentials: {
-      accessKeyId: "c7ce7d50c59d423511191a0ff1d3065e",
-      secretAccessKey:
-        "53f52c2218609145eb0b3c20dd178981c3ccec6d062d33e6fc4de20c6846fb58",
+      accessKeyId: process.env.NEXT_PUBLIC_R2_ACCESS_KEY!,
+      secretAccessKey: process.env.NEXT_PUBLIC_R2_SECRET_KEY!,
     },
   });
 
@@ -52,7 +46,7 @@ const uploadFileToR2 = async ({
 
     await s3Client.send(command);
 
-    const url = `https://42995ddbe3fdf60d4dd3fa0f60343689.r2.cloudflarestorage.com/${bucketName}/${fileName}`;
+    const url = `${process.env.NEXT_PUBLIC_R2_ENDPOINT}/${bucketName}/${fileName}`;
     return url;
   } catch (error) {
     console.error("R2 upload error:", error);
